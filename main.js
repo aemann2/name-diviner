5b0401a665f9640ec0d22ba372cfc9341012669d
Adam Mann <aemann2@gmail.com>
2021-02-16 16:51:26 -0800

    Pass entry data to results page

--- main.js
+++ main.js
@@ -1,78 +1,125 @@
-const nameForm = document.querySelector('form');
+const nameForm = document.querySelector('#name-form');
 
 nameForm.addEventListener('submit', submitSearch);
 
-function submitSearch(e) {
+const storage = window.localStorage;
+
+async function submitSearch(e) {
   e.preventDefault();
 
   const fullName = this.name.value.split(' ');
   const firstName = fullName[0];
   const lastName = fullName[fullName.length - 1];
 
-  // Axios request
-  axios.get(`https://api.nationalize.io?name=${firstName}`).then((response) => {
-    console.log(response.data);
-  });
+  // Axios requests to both APIs using .all
+
+  await axios
+    .all([
+      await axios.get(`https://api.nationalize.io?name=${firstName}`),
+      axios.get(`https://api.agify.io?name=${firstName}`),
+    ])
+    .then(
+      axios.spread((data1, data2) => {
+        storage.setItem('country', JSON.stringify(data1.data));
+        storage.setItem('age', JSON.stringify(data2.data));
+      })
+    )
+    .catch((err) => {
+      if (err.response) {
+        console.log(err.response);
+      } else if (err.request) {
+        console.log(err.request);
+      } else {
+        console.log(err);
+      }
+    });
 
   // Fetch request
 
   // I need to work around the CORS problem. Here's a thread on getting it working:
   // https://github.com/Rob--W/cors-anywhere/issues/301
 
-  fetch(
-    `https://cors-anywhere.herokuapp.com/https://binaryfog-last-name-origin-v1.p.rapidapi.com/api/LastName/origin?lastName=${lastName}`,
-    {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': '56136daf19msh385e4199ffdb756p10ac0fjsn9721600950a7',
-        'x-rapidapi-host': 'binaryfog-last-name-origin-v1.p.rapidapi.com',
-      },
-    }
-  )
-    .then((response) => {
-      /// only return the JSON object once the status is 200
-      if (response.status === 200) {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  //  fetch(
+  //     `https://cors-anywhere.herokuapp.com/https://binaryfog-last-name-origin-v1.p.rapidapi.com/api/LastName/origin?lastName=${lastName}`,
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         'x-rapidapi-key': '56136daf19msh385e4199ffdb756p10ac0fjsn9721600950a7',
+  //         'x-rapidapi-host': 'binaryfog-last-name-origin-v1.p.rapidapi.com',
+  //       },
+  //     }
+  //   )
+  //     .then((response) => {
+  //       /// only return the JSON object once the status is 200
+  //       if (response.status === 200) {
+  //         return response.json();
+  //       }
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
 
-  // XHR request
-  let httpRequest = new XMLHttpRequest();
-
-  httpRequest.onreadystatechange = returnXHRData;
-  httpRequest.open('GET', `https://api.agify.io?name=${firstName}`, true);
-  httpRequest.send();
-
-  function returnXHRData() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        // must use JSON.parse for XHR requests, since XHR returns JSON as a string
-        console.log(JSON.parse(httpRequest.responseText));
-      } else {
-        alert('There was a problem with the request.');
-      }
-    }
-  }
+  newPage();
 }
 
-// Here's what the FETCH response looks like for above:
+async function newPage() {
+  await submitSearch;
+  window.location.href = '/results.html';
+}
 
-// {name: "michael", country: Array(3)}
-// country: Array(3)
-// 0:
-// country_id: "US"
-// probability: 0.08986482266532715
-// __proto__: Object
-// 1:
-// country_id: "AU"
-// probability: 0.05976757527083082
-// __proto__: Object
-// 2:
-// country_id: "NZ"
-// probability: 0.04666974820852911}
+// XHR request
+// let xhr = new XMLHttpRequest();
+
+// xhr.open('GET', `https://api.agify.io?name=${firstName}`, true);
+
+// xhr.onload = function () {
+//   if (this.status === 200) {
+//     // must use JSON.parse for XHR requests, since XHR returns JSON as a string
+//     console.log(JSON.parse(this.responseText));
+//   } else {
+//     // error handling for application-level errors
+//     console.log('There was a problem with the request');
+//   }
+// };
+// // error handling for network errors
+// xhr.onerror = function () {
+//   console.log('Request Error...');
+// };
+
+// xhr.send();
+
+// Axios requests
+// await axios
+//   .get(`https://api.nationalize.io?name=${firstName}`)
+//   .then((response) => {
+//     storage.setItem('country', JSON.stringify(response.data));
+//     console.log(response.data);
+//   })
+//   .catch((err) => {
+//     if (err.response) {
+//       console.log(err.response);
+//     } else if (err.request) {
+//       console.log(err.request);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+
+// await axios
+//   .get(`https://api.agify.io?name=${firstName}`)
+//   .then((response) => {
+//     storage.setItem('age', JSON.stringify(response.data));
+//     console.log(response.data);
+//   })
+//   .catch((err) => {
+//     if (err.response) {
+//       console.log(err.response);
+//     } else if (err.request) {
+//       console.log(err.request);
+//     } else {
+//       console.log(err);
+//     }
+//   });
