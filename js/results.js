17196b02af1a5397d26c1561ca842964401b73bd
Adam Mann <aemann2@gmail.com>
2021-02-18 20:47:35 -0800

    added error handling for results page

--- js/results.js
+++ js/results.js
@@ -7,21 +7,19 @@ const ageText = document.querySelector('#age');
 const nameOriginText = document.querySelector('#name-origin');
 
 // assigning variables to values from localStorage (which are set in entry.js)
-let country = localStorage.getItem('country');
-let age = localStorage.getItem('age');
-let origin = localStorage.getItem('origin');
+let {
+  country: [country],
+} = JSON.parse(localStorage.getItem('country'));
+const { age } = JSON.parse(localStorage.getItem('age'));
+const { origin } = JSON.parse(localStorage.getItem('origin'));
 
-// converting to JSON
-country = JSON.parse(country).country[0].country_id;
-age = JSON.parse(age);
-origin = JSON.parse(origin);
-
-// putting data into the DOM
-nationalityText.innerHTML = `The Oracle believes you live in ${isoCountries[country]}.`;
-ageText.innerHTML = `The Oracle believes you are ${age.age} years old.`;
-nameOriginText.innerHTML = `The Oracle believes your last name is ${origin.origin}.`;
-
-// Console logs
-// console.log(country);
-// console.log(age.age);
-// console.log(origin.origin);
+// putting data into the DOM...error handling in case the API returns nothing
+nationalityText.innerHTML = country
+  ? `The Oracle believes you live in ${isoCountries[country.country_id]}.`
+  : `The Oracle can't figure this one out...`;
+ageText.innerHTML = age
+  ? `The Oracle believes you are ${age} years old.`
+  : `The Oracle is stumped...`;
+nameOriginText.innerHTML = origin
+  ? `The Oracle believes your last name is ${origin}.`
+  : `The Oracle isn't sure...`;
